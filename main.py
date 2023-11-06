41056066e60df5a878957f365899e6b37caa712a
Gihong Yim <gyim@c2r8s1.42seoul.kr>
2023-11-06 15:42:32 +0900

    asdf

--- main.py
+++ main.py
@@ -8,29 +8,37 @@ from predict import predict_price
 from linear_regression import Linear_regression
 
 def main():
-    data = pd.read_csv('data.csv')
+    try:
+        data = pd.read_csv('data.csv')
+    except Exception as e:
+        print(f"{e}")
+        return
     km =  data.loc[:,'km'].astype('float')
     price = data.loc[:, 'price'].astype('float')
     model = Linear_regression(x=km, y=price)
-    model.train(epochs=10000, lr=0.01)
-    # theta_history = model.theta_history
-    # theta0, theta1 = model.theta0, model.theta1
+    model.train(epochs=2000, lr=0.03)
     fig = plt.figure()
-    sns.scatterplot(x=km, y=price)
     
+    epoch = 1
     for theta0, theta1 in model.theta_history:
         model.theta0 = theta0
         model.theta1 = theta1
-        plt.xlim(model.x_min, model.x_max)
-        plt.ylim(model.y_min, model.y_max)
-        data_x = np.linspace(np.min(km), np.max(km), num=1000)
-        data_y = [model.predict(x) for x in data_x]
-        fig.clear()
-        sns.lineplot(x=data_x, y=data_y)
-        plt.pause(0.01)
+        if epoch % 20 == 0 or epoch == len(model.theta_history):
+            plt.xlim(model.x_min, model.x_max)
+            plt.ylim(model.y_min, model.y_max)
+            data_x = np.linspace(np.min(km), np.max(km), num=1000)
+            data_y = [model.predict(x) for x in data_x]
+            fig.clear()
+            sns.scatterplot(x=km, y=price)
+            sns.lineplot(x=data_x, y=data_y).set(title=f"{epoch} epoch")
+            plt.pause(0.0001)
+        epoch += 1
     plt.show()    
     plt.figure()
-    sns.lineplot(model.loss_history)
+    sns.lineplot(model.loss_history).set(title="loss")
+    plt.show()
+    error_dist = [y - model.predict(x) for x, y in zip(km, price)]
+    sns.histplot(data=error_dist).set(title="error : y - f(x)")
     plt.show()
     
     
